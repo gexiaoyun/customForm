a66ad9b21a4cb89f394ecedb9327aa3f925fd3fa
葛小云 <gexiaoyun@users.noreply.github.com>
2020-03-06 10:02:06 +0800

    remove node_module

--- node_modules/rc-table/es/context/TableContext.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { GetComponent } from '../interface';
-export interface TableContextProps {
-    prefixCls: string;
-    getComponent: GetComponent;
-    scrollbarSize: number;
-}
-declare const TableContext: React.Context<TableContextProps>;
-export default TableContext;
--- node_modules/rc-table/es/hooks/useStickyOffsets.d.ts
+++
@@ -1,6 +0,0 @@
-import { StickyOffsets } from '../interface';
-/**
- * Get sticky column offset width
- */
-declare function useStickyOffsets(colWidths: number[], columCount: number): StickyOffsets;
-export default useStickyOffsets;
